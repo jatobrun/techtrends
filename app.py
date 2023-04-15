aae49e28c134bea68b4d92b482b35ec65d56889c
Jamil Torres <jatobrun@espol.edu.ec>
2023-04-15 12:51:26 -0500

    Finish Step 1

---
+++ app.py
@@ -0,0 +1,98 @@
+import logging
+import sqlite3
+
+from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
+from werkzeug.exceptions import abort
+
+# Function to get a database connection.
+# This function connects to database with the name `database.db`
+def get_db_connection():
+    connection = sqlite3.connect('database.db')
+    connection.row_factory = sqlite3.Row
+    return connection
+
+# Function to get a post using its ID
+def get_post(post_id):
+    connection = get_db_connection()
+    post = connection.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    connection.close()
+    return post
+
+# Define the Flask application
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
+
+# Define the main route of the web application 
+@app.route('/')
+def index():
+    connection = get_db_connection()
+    posts = connection.execute('SELECT * FROM posts').fetchall()
+    connection.close()
+    return render_template('index.html', posts=posts)
+
+# Define how each individual article is rendered 
+# If the post ID is not found a 404 page is shown
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    if post is None:
+      app.logger.info(f"Article with id {post_id} does not exists")
+      return render_template('404.html'), 404
+    else:
+      app.logger.info(f"Article {post['title']} retrieve")
+      return render_template('post.html', post=post)
+
+# Define the About Us page
+@app.route('/about')
+def about():
+    app.logger.info("The page About was render")
+    return render_template('about.html')
+
+# Define the post creation functionality 
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            connection = get_db_connection()
+            connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            connection.commit()
+            connection.close()
+
+            app.logger.info(f"The Article with title {title} was create successfully")
+
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
+
+# Define the healthcheck route of the web application 
+@app.route('/healthz')
+def status():
+    response = {"result": "OK - healthy"}
+    return jsonify(response)
+
+# Define the metrics route of the web application 
+@app.route('/metrics')
+def metrics():
+    connection = get_db_connection()
+    posts = connection.execute('SELECT * FROM posts').fetchall()
+
+    
+    response = {
+        "db_connection_count": "",
+        "post_count": len(posts)
+    }
+
+    connection.close()
+    return jsonify(response)
+
+# start the application on port 3111
+if __name__ == "__main__":
+   logging.basicConfig(level=logging.DEBUG, format='%(levelname)s:%(name)s:%(asctime)s, %(message)s', datefmt='%d/%m/%Y, %H:%M:%S')
+   app.run(host='0.0.0.0', port='3111')
